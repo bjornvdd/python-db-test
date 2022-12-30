ce61b24c1456d64b32dd0caee78b53738466dd7e
Bjorn Van den Dungen <r0889357@student.thomasmore.be>
2022-12-30 15:03:26 +0100

    Update schemas.py

--- myproject/schemas.py
+++ myproject/schemas.py
@@ -1,35 +1,53 @@
 from pydantic import BaseModel
 
 
-class ItemBase(BaseModel):
-    title: str
-    description: str | None = None
-
-
-class ItemCreate(ItemBase):
-    pass
-
-
-class Item(ItemBase):
-    id: int
-    owner_id: int
-
-    class Config:
-        orm_mode = True
-
-
 class UserBase(BaseModel):
+    id: int | None = None
+    first_name: str
+    last_name: str
     email: str
+    number: int | None = "Invalid"
+    birth_date: str
+
+
+class TeamBase(BaseModel):
+    id: int | None = None
+    team_name: str
+    captain_team: str
+    played_games: int
+    ranking: int
+
+
+class StadiumBase(BaseModel):
+    id: int | None = None
+    stadium_name: str
+    city: str
+    capacity: float
+    age: int
+
+
+class TeamCreate(TeamBase):
+    pass
 
 
 class UserCreate(UserBase):
     password: str
 
 
-class User(UserBase):
-    id: int
-    is_active: bool
-    items: list[Item] = []
+class StadiumCreate(StadiumBase):
+    pass
 
+
+class Team(TeamBase):
+    class Config:
+        orm_mode = True
+
+
+class User(UserBase):
+    class Config:
+        orm_mode = True
+
+
+class Stadium(StadiumBase):
     class Config:
         orm_mode = True
